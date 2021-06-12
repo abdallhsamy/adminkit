57423491bc841a7079b760ea65cc94c4c5804caa
Abdallah Samy <abdallhsamy2011@gmail.com>
2021-06-12 16:52:05 +0200

    add translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -18,5 +18,18 @@ return [
         'forgot_password'   => 'Forgot Password?',
         'remember_me'   => 'Remember Me',
         'login' => 'Login',
+    ],
+    'register' => [
+        'register'  => 'Register',
+        'email'  => 'Email',
+        'full_name' => 'Full Name',
+        'enter_full_name'   => 'Enter Full Name',
+        'enter_email_address'   => 'Enter Email address',
+        'password'  => 'Password',
+        'set_account_password'  => 'Set account password',
+        'confirm_password'  => 'Confirm Password',
+        'confirm_account_password'  => 'Confirm account password',
+        'already_have_an_account'   => 'Already have an account ?',
+        'sign_in'    => 'SignIn',
     ]
 ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.auth_app')
 @section('title')
-    Register
+{{ __('auth.register.register') }}
 @endsection
 @section('content')
     <div class="card card-primary">
-        <div class="card-header"><h4>Register</h4></div>
+        <div class="card-header"><h4>{{ __('auth.register.register') }}</h4></div>
 
         <div class="card-body pt-1">
             <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
@@ -12,12 +12,11 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="first_name">Full Name:</label><span
-                                    class="text-danger">*</span>
+                            <label for="first_name">{{ __('auth.register.full_name') }}:</label><span class="text-danger">*</span>
                             <input id="firstName" type="text"
                                    class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
                                    name="name"
-                                   tabindex="1" placeholder="Enter Full Name" value="{{ old('name') }}"
+                                   tabindex="1" placeholder="{{ __('auth.register.enter_full_name') }}" value="{{ old('name') }}"
                                    autofocus required>
                             <div class="invalid-feedback">
                                 {{ $errors->first('name') }}
@@ -26,11 +25,10 @@
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="email">Email:</label><span
-                                    class="text-danger">*</span>
+                            <label for="email">{{ __('auth.register.email') }}:</label><span class="text-danger">*</span>
                             <input id="email" type="email"
                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                   placeholder="Enter Email address" name="email" tabindex="1"
+                                   placeholder="{{ __('auth.register.enter_email_address') }}" name="email" tabindex="1"
                                    value="{{ old('email') }}"
                                    required autofocus>
                             <div class="invalid-feedback">
@@ -40,12 +38,10 @@
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="password" class="control-label">Password
-                                :</label><span
-                                    class="text-danger">*</span>
+                            <label for="password" class="control-label">{{ __('auth.register.password') }}:</label><span class="text-danger">*</span>
                             <input id="password" type="password"
                                    class="form-control{{ $errors->has('password') ? ' is-invalid': '' }}"
-                                   placeholder="Set account password" name="password" tabindex="2" required>
+                                   placeholder="{{ __('auth.register.set_account_password') }}" name="password" tabindex="2" required>
                             <div class="invalid-feedback">
                                 {{ $errors->first('password') }}
                             </div>
@@ -54,9 +50,9 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="password_confirmation"
-                                   class="control-label">Confirm Password:</label><span
+                                   class="control-label">{{ __('auth.register.confirm_password') }}:</label><span
                                     class="text-danger">*</span>
-                            <input id="password_confirmation" type="password" placeholder="Confirm account password"
+                            <input id="password_confirmation" type="password" placeholder="{{ __('auth.register.confirm_account_password') }}"
                                    class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid': '' }}"
                                    name="password_confirmation" tabindex="2">
                             <div class="invalid-feedback">
@@ -66,9 +62,7 @@
                     </div>
                     <div class="col-md-12 mt-4">
                         <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                                Register
-                            </button>
+                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">{{ __('auth.register.register') }}</button>
                         </div>
                     </div>
                 </div>
@@ -76,6 +70,6 @@
         </div>
     </div>
     <div class="mt-5 text-muted text-center">
-        Already have an account ? <a href="{{ route('login') }}">SignIn</a>
+        {{ __('auth.register.already_have_an_account') }} <a href="{{ route('login') }}">{{ __('auth.register.sign_in') }}</a>
     </div>
 @endsection
