e434da4f69ec43802824deafeec7bc25e8aeeedc
Abdallah Samy <abdallhsamy2011@gmail.com>
2021-06-12 15:37:57 +0200

    init generator

--- config/app.php
+++ config/app.php
@@ -228,6 +228,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
     ],
 
 ];
