c6701261834226f751330c39f135d8509472dfde
Abdallah Samy <abdallhsamy2011@gmail.com>
2021-06-14 14:38:23 +0200

    user crud

--- resources/views/users/show_fields.blade.php
+++ resources/views/users/show_fields.blade.php
@@ -15,22 +15,3 @@
     {!! Form::label('email', __('models/users.fields.email').':') !!}
     <p>{{ $user->email }}</p>
 </div>
-
-<!-- Password Field -->
-<div class="form-group">
-    {!! Form::label('password', __('models/users.fields.password').':') !!}
-    <p>{{ $user->password }}</p>
-</div>
-
-<!-- Password Confirmation Field -->
-<div class="form-group">
-    {!! Form::label('password_confirmation', __('models/users.fields.password_confirmation').':') !!}
-    <p>{{ $user->password_confirmation }}</p>
-</div>
-
-<!-- Role Id Field -->
-<div class="form-group">
-    {!! Form::label('role_id', __('models/users.fields.role_id').':') !!}
-    <p>{{ $user->role_id }}</p>
-</div>
-
